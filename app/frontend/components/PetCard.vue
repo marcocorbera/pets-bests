92938ee2ff48d81fa8cace279777d7d0c3db96dc
Marco Corbera Santillan <marco.spaceag@gmail.com>
2022-10-29 18:14:43 -0500

    added petcard component

---
+++ app/frontend/components/PetCard.vue
@@ -0,0 +1,53 @@
+<template>
+    <v-card
+        :width="{
+            '100%': $vuetify.breakpoint.smAndDown,
+            '50%': $vuetify.breakpoint.mdAndUp
+        }"
+        class="d-flex flex-column my-3"
+        >
+        <v-img
+            :src="item.photo"
+            class="white--text align-end"
+            width="100%"
+            height="150"
+        >
+            <v-card-title>
+            {{ item.name }}
+            </v-card-title>
+        </v-img>
+        <v-card-actions
+            class="justify-center"
+        >
+            <v-btn
+            color="primary"
+            text
+            >
+            <v-icon>
+                mdi-heart
+            </v-icon>
+            Salud
+            </v-btn>
+            <v-btn
+            color="primary"
+            text
+            >
+            <v-icon>
+                mdi-pencil
+            </v-icon>
+            Editar
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+<script>
+export default {
+    name: 'PetCard',
+    props: {
+        item: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
--- app/frontend/pages/index.vue
+++ app/frontend/pages/index.vue
@@ -1,60 +1,27 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <v-card
-        v-for="item,key in items"
+      <pet-card
+        v-for="item, key in items"
         :key="key"
-        :width="{
-          '100%': $vuetify.breakpoint.smAndDown,
-          '50%': $vuetify.breakpoint.mdAndUp
-        }"
-        class="d-flex flex-column my-3"
-      >
-        <v-img
-          :src="item.photo"
-          class="white--text align-end"
-          width="100%"
-          height="150"
-        >
-          <v-card-title>
-            {{ item.name }}
-          </v-card-title>
-        </v-img>
-        <v-card-actions
-         class="justify-center"
-        >
-          <v-btn
-            color="primary"
-            text
-          >
-            <v-icon>
-              mdi-heart
-            </v-icon>
-            Salud
-          </v-btn>
-          <v-btn
-            color="primary"
-            text
-          >
-            <v-icon>
-              mdi-pencil
-            </v-icon>
-            Editar
-          </v-btn>
-        </v-card-actions>
-
-      </v-card>
+        :item="item"
+      />
     </v-col>
   </v-row>
 </template>
 
 <script>
 import ApiClient from '~/services/api-client'
+import PetCard from '~/components/PetCard'
+
 export
  default {
   name: 'IndexPage',
   created () {
     this.getPets()
+  },
+  components: {
+    PetCard
   },
   data () {
     return {
